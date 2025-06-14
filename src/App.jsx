0f497fc1e66a97683821564a4921da2597080d4c
Anvith shetty <161817374+Anvithshetty17@users.noreply.github.com>
2025-06-14 13:22:12 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Logo from './components/True_Ice_cream/logo';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+     
+      
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Header.css';
+import { FaShoppingBag, FaBars } from 'react-icons/fa';
+
+const Header = () => {
+    return (
+        <header className="header">
+            {/* Updated Logo to use the actual SVG */}
+            <a href="/" className="logo" aria-label="Mr. Pops">
+                <img src="/logo.png" alt="" />
+            </a>
+
+
+
+            <div className="nav-right">
+                <div className="nav-item cart">
+                    <div className="icon-box">
+                        <FaShoppingBag />
+                    </div>
+                    <div className="count-box">0</div>
+                </div>
+                <div className="nav-item">Flavours</div>
+                <div className="nav-item">
+                    Menu <FaBars className="menu-icon" />
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
